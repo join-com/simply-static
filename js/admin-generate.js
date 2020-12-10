01fb868bbaa9993c1c06a274a1dedcf85095a9ed
Andrii Dutchak <opora.foto@gmail.com>
2020-12-10 21:32:59 +0200

    Added back ping in admin section.

--- js/admin-generate.js
+++ js/admin-generate.js
@@ -35,11 +35,9 @@ jQuery( document ).ready( function( $ ) {
 		// send action now
 		send_action_to_archive_manager( action );
 		// set loop for pinging server
-		/*
 		refreshTimer = setInterval( function() {
 			send_action_to_archive_manager( 'ping' );
 		}, REFRESH_EVERY_X_SECONDS * 1000 );
-		*/
 	}
 
 	// where action is one of 'start', 'continue', 'cancel'
